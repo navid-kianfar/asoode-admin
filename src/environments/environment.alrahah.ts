63adbb8fbd516d66cc9b1ab2bf39b43d763ac152
Navid Kianfar <nvd.kianfar@gmail.com>
2020-11-23 14:54:39 +0330

    admin with multi environment

---
+++ src/environments/environment.alrahah.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  api_endpoint: 'https://api.al-rahah.com/v2',
+  googleOauth: 'https://api.al-rahah.com/oauth/google-login/admin',
+};
---
+++ src/environments/environment.ciblsoft.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  api_endpoint: 'https://api.ciblsoft.com/v2',
+  googleOauth: 'https://api.ciblsoft.com/oauth/google-login/admin',
+};
