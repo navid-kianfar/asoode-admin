5ac2374ef240a99329fe2dc45347bb061e24457d
Navid Kianfar <nvd.kianfar@gmail.com>
2021-05-09 14:34:21 +0300

    .

--- src/environments/environment.drethos.ts
+++ src/environments/environment.drethos.ts
@@ -1,9 +1,9 @@
 export const environment = {
   lang: 'en',
   production: true,
-  direct_endpoint: `https://admin.asoode.drethos.com`,
-  api_endpoint: `https://api.asoode.drethos.com/v2`,
-  socket_endpoint: `https://socket.asoode.drethos.com`,
-  googleOauth: `https://api.asoode.drethos.com/oauth/google-login/admin`,
+  direct_endpoint: `https://admin-asoode.drethos.com`,
+  api_endpoint: `https://api-asoode.drethos.com/v2`,
+  socket_endpoint: `https://socket-asoode.drethos.com`,
+  googleOauth: `https://api-asoode.drethos.com/oauth/google-login/admin`,
   ga: 'G-Y3H4HMTWZG',
 };
